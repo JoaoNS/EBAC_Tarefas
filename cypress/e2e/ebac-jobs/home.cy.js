491867fd29eba2d0a367eb3cbd86139d479f36f7
JoaoNS <joaopnunes252@gmail.com>
2024-07-17 18:19:31 -0300

    bug-fix

--- cypress/e2e/ebac-jobs/home.cy.js
+++ cypress/e2e/ebac-jobs/home.cy.js
@@ -6,17 +6,18 @@ describe("Teste para a home", () => {
   });
 
   it("Deve incluir informações no formulário", () => {
-    cy.get('input[type="text"]').type("Carlos Alberto");
-    cy.get('input[type="email"]').type("teste@gmail.com");
+    cy.get('input[type="text"]').type("Pablo");
+    cy.get('input[type="email"]').type("testeP@gmail.com");
     cy.get('input[type="tel"]').type("11912345678");
     cy.contains("Adicionar").click();
     cy.screenshot("teste-incluir");
+    cy.wait(4000);
   });
 
   it("Deve alterar a informação da lista", () => {
     cy.get(":nth-child(2) > .sc-gueYoa > .edit").click();
     cy.get('input[type="text"]').clear().type("Jose Pedro");
-    cy.get('input[type="email"]').clear().type("testen2.com.br");
+    cy.get('input[type="email"]').clear().type("teste2@gmail.com");
     cy.get('input[type="tel"]').clear().type("11912345678");
     cy.get(".alterar").click();
     cy.screenshot("teste-editar");
